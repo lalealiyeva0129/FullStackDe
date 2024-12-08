7f94a2f4716ad633776c5bcbc1d6008322a4b1e5
lalealiyeva0129 <lalealiyeva744@gmail.com>
2024-12-08 16:30:19 +0400

    firstcommit2

---
+++ Js.1/app1.js
@@ -0,0 +1,20 @@
+let reqem=prompt("Zehmet olmasa, 1 ile 5 arasinda bir reqem daxil edin:");
+
+switch(reqem){
+      case "1":
+            console.log("Daxil etdiyiniz reqem"+ reqem + "-dir");
+      break;
+      case "2":
+            console.log("Daxil etdiyiniz reqem"+ reqem + "dir");
+      break;
+      case "3":
+            console.log("Daxil etdiyiniz reqem"+ reqem + "dir");
+      break;
+      case "4":
+            console.log("Daxil etdiyiniz reqem"+ reqem +"dir");
+      break;
+      case "5":
+            console.log("Daxil etdiyiniz reqem"+ reqem +"dir");
+      default:
+            console.log("Daxil etdiyiniz reqem 1 ile 5 arasinda deyil.");
+}
