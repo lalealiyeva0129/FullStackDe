ca05de69542be7571e5fa4b5eb3b18cd8536a44a
lalealiyeva0129 <lalealiyeva744@gmail.com>
2024-12-10 21:19:46 +0400

    firstcommit4

--- Js.1/app1.js
+++ Js.1/app1.js
@@ -1,7 +1,7 @@
 /* 
 ! Swich_case  if_else ile eyni funksiani dasiyir.AMMA if daha cox istifade edilir.
 */ 
-/*
+
 let reqem=prompt("Zehmet olmasa, 1 ile 5 arasinda bir reqem daxil edin:");
 switch(reqem){
       case "1":
@@ -21,18 +21,18 @@ switch(reqem){
       default:
             console.log("Daxil etdiyiniz reqem 1 ile 5 arasinda deyil.");
 }
-*/
+
 
 /*
   ? 1__10 qeder yazdir.Ve onlarin cemini de tap
  */
- /*let sum=0;
+ let sum=0;
 for(i=0;i<=10;i++){
       sum=sum+i;
    console.log(i);
 }
 console.log(sum);
-*/
+
 /*
  ? 1-dən 100-ə qədər olan cüt rəqəmləri ekranda göstərən bir JavaScript proqramı yaz.
 */ 
@@ -53,3 +53,11 @@ for(i=0;i<100;i++){
       }
 }
 
+// ? Vurma cedveli 
+
+for(i=1;i<=10;i++){
+      for(j=1;j<=10;j++){
+            console.log(i +"*"+j+"="+i*j);
+      }
+      console.log( "..........");
+}
